33284b1744a5d62d87e47ac31491299ec9c6afda
FelipeCordeiroC <felipeclassecordeiro@hotmail.com>
2024-09-09 21:05:25 -0300

    Add files via upload

---
+++ exc11.prova.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+
+    void calculo(float valor, float ICMS) {
+        float desconto = valor * ICMS;
+        float resultado = valor - desconto;
+        int porc = ICMS * 100;
+        printf("\nO valor final é de R$%.2f\n\nValor original: R$%.2f\nDesconto de ICMS: %d%%", resultado, valor, porc);
+        
+}
+
+int main()
+{
+ 
+    float valor;
+    float ICMS;
+    
+    printf("Digite o valor:\t");
+    scanf("%f", &valor);
+    
+    if (valor <= 1000) {
+        ICMS = 0.05;
+    } else if(valor > 1000 && valor <= 5000) {
+        ICMS = 0.1;
+    } else if(valor > 5000) {
+        ICMS = 0.15;
+    }
+    
+    calculo(valor,ICMS);
+
+    return 0;
+}
---
+++ exc14.prova.c
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    int opt;
+    printf("Que turno é no momento?\n{1} Manhã\n{2} Tarde\n{3} Noite\nResposta: ");
+    scanf("%d", &opt);
+    
+    switch(opt) {
+        case 1: 
+            printf("Bom dia flor do dia! Seja bem-vindo(a)");
+            break;
+            
+        case 2:
+            printf("Boa tarde campeão! Seja bem-vindo(a)");
+            break;
+            
+        case 3:
+            printf("Boa noite querido(a)! Seja bem-vindo(a)");
+            break;
+            
+        default: 
+            printf("Resposta inválida, tente novamente");
+            break;
+    }
+
+    return 0;
+}
